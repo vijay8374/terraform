9e786e595d7b87145b6522f6d8c03e8ab8baf7c3
vijay <barishettivijay123@gmail.com>
2023-02-20 13:25:21 +0530

    user-devops

--- terraform-main/2.build_server/scripts/install-maven.sh
+++ terraform-main/2.build_server/scripts/install-maven.sh
@@ -29,13 +29,13 @@ hostnamectl set-hostname build-server
 HEADING "Creating DevOps User"
 
 # add the user devops
-useradd vijju
+useradd devops
 # set password : the below command will avoid re entering the password
-echo "vijay123" | passwd --stdin devops
-echo "vijay123" | passwd --stdin ec2-user
+echo "devops" | passwd --stdin devops
+echo "devops" | passwd --stdin ec2-user
 # modify the sudoers file at /etc/sudoers and add entry
-echo 'vijay123     ALL=(ALL)      NOPASSWD: ALL' | sudo tee -a /etc/sudoers
-echo 'vijju     ALL=(ALL)      NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+echo "devops"     ALL=(ALL)      NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+echo "devops"     ALL=(ALL)      NOPASSWD: ALL' | sudo tee -a /etc/sudoers
 # this command is to add an entry to file : echo 'PasswordAuthentication yes' | sudo tee -a /etc/ssh/sshd_config
 # the below sed command will find and replace words with spaces "PasswordAuthentication no" to "PasswordAuthentication yes"
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
